e5d7ed518d7da7e7dc3ce894371a0901f258f93b
Felix <hongkongfelix@gmail.com>
2019-10-23 23:39:52 +0800

    Fix pandas slice warning

--- map_editor/hexMap.py
+++ map_editor/hexMap.py
@@ -149,27 +149,30 @@ class hexMap:
                     for col in swap_cols:
                         df_new_id = self.hex_df.index[(self.hex_df.xid == new_x) & 
                                                       (self.hex_df.yid == new_y)][0]
-                        old_val = self.hex_df[col].iloc[df_old_id].copy()
-                        self.hex_df[col].iloc[df_old_id] = self.hex_df[col]\
-                            .iloc[df_new_id].copy()
-                        self.hex_df[col].iloc[df_new_id] = old_val
+                        old_val = self.hex_df[col].iloc[df_old_id]
+                        new_temp_col = self.hex_df[col].copy()
+                        new_temp_col.iloc[df_old_id] = self.hex_df[col]\
+                            .iloc[df_new_id]
+                        new_temp_col.iloc[df_new_id] = old_val
+
+                        self.hex_df[col] = new_temp_col
                     
                 else:
                     # Change to new hex
                     df_new_id = self.grid_df.index[(self.grid_df.xid == new_x) & 
                                                    (self.grid_df.yid == new_y)]
-                    new_wgs_x = self.grid_df.geometry.iloc[df_new_id].x
-                    new_wgs_y = self.grid_df.geometry.iloc[df_new_id].y
+                    new_wgs_x = self.grid_df.geometry.iloc[df_new_id].x.values[0]
+                    new_wgs_y = self.grid_df.geometry.iloc[df_new_id].y.values[0]
 
                     new_wgs_polygon = self._draw_hex(new_wgs_x, new_wgs_y,
                                                      self.hex_width, 'wgs')
 
                     # Replace into table
-                    self.hex_df['xid'].iloc[df_old_id] = new_x
-                    self.hex_df['yid'].iloc[df_old_id] = new_y
-                    self.hex_df['x'].iloc[df_old_id] = new_wgs_x
-                    self.hex_df['y'].iloc[df_old_id] = new_wgs_y
-                    self.hex_df['geometry'].iloc[df_old_id] = new_wgs_polygon
+                    self.hex_df.at[df_old_id, 'xid'] = new_x
+                    self.hex_df.at[df_old_id, 'yid'] = new_y
+                    self.hex_df.at[df_old_id, 'x'] = new_wgs_x
+                    self.hex_df.at[df_old_id, 'y'] = new_wgs_y
+                    self.hex_df.at[df_old_id, 'geometry'] = new_wgs_polygon
 
                 return True
 
@@ -181,7 +184,7 @@ class hexMap:
             return False
 
         
-    def get_genPandas(self):
+    def get_geoPandas(self):
         """
         Get the geoPandas object of whole hexagon map
 
