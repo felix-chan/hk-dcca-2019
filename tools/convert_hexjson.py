c4a07f8152931719d58caa429f5336100fc717ef
Felix <hongkongfelix@gmail.com>
2019-12-04 00:15:49 +0800

    Add the handling of cube and offset even-q

--- tools/convert_hexjson.py
+++ tools/convert_hexjson.py
@@ -26,3 +26,23 @@ def convert_to_axial(xid, yid):
     v = list(max_y - np.array(yid))
     r = [int((v1 - q1) / 2) for v1, q1 in zip(v, q)]
     return (q, r)
+
+def double_to_cube(row, col):
+    """
+    Function to convert double to cube coordinate
+    https://www.redblobgames.com/grids/hexagons/#conversions-doubled
+    """
+    v_col = col if (row + col % 2) == 0 else col + 1
+    x = v_col
+    z = int((row - v_col) / 2)
+    y = -x - z
+    return (x, y, z)
+
+def cube_to_evenq(x, y, z):
+    """
+    Function to convert cube to even-q offset coordinate
+    https://www.redblobgames.com/grids/hexagons/#conversions-offset
+    """
+    col = x
+    row = int(z + (x + x&1) / 2)
+    return (col, row)
